3dfcbafc22ef07bed6e3b5038e52794850144774
YouTy6bl4 <iura.andrianov@Yandex.ua>
2015-07-11 22:21:20 +0300

    Update 1
    Переписан контроллер для модуля в админпанели. Re-designed the
    controller for the module in the admin panel.

--- src/admin/controller/payment/unitpay.php
+++ src/admin/controller/payment/unitpay.php
@@ -6,96 +6,96 @@ class ControllerPaymentUnitpay extends Controller {
 
 		$this->load->language('payment/unitpay');
 
-		$this->document->setTitle = $this->language->get('heading_title');
+		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->load->model('setting/setting');
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && ($this->validate())) {
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
 			$this->load->model('setting/setting');
 
 			$this->model_setting_setting->editSetting('unitpay', $this->request->post);
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
-			$this->redirect(HTTPS_SERVER . 'index.php?route=extension/payment&token=' . $this->session->data['token']);
+			$this->response->redirect(HTTPS_SERVER . 'index.php?route=extension/payment&token=' . $this->session->data['token'], 'SSL');
 		}
 
-		$this->data['heading_title'] = $this->language->get('heading_title');
+		$data['heading_title'] = $this->language->get('heading_title');
 
-		$this->data['text_enabled'] = $this->language->get('text_enabled');
-		$this->data['text_disabled'] = $this->language->get('text_disabled');
-		$this->data['text_all_zones'] = $this->language->get('text_all_zones');
-		$this->data['text_liqpay'] = $this->language->get('text_liqpay');
-		$this->data['text_card'] = $this->language->get('text_card');
-		$this->data['text_yes'] = $this->language->get('text_yes');
-		$this->data['text_no'] = $this->language->get('text_no');
+		$data['text_enabled'] = $this->language->get('text_enabled');
+		$data['text_disabled'] = $this->language->get('text_disabled');
+		$data['text_all_zones'] = $this->language->get('text_all_zones');
+		$data['text_liqpay'] = $this->language->get('text_liqpay');
+		$data['text_card'] = $this->language->get('text_card');
+		$data['text_yes'] = $this->language->get('text_yes');
+		$data['text_no'] = $this->language->get('text_no');
          		
 		
-		$this->data['entry_login'] = $this->language->get('entry_login');
-		$this->data['entry_password1'] = $this->language->get('entry_password1');
+		$data['entry_login'] = $this->language->get('entry_login');
+		$data['entry_password1'] = $this->language->get('entry_password1');
 		
 
 		// URL
-		$this->data['copy_result_url'] 	= HTTP_CATALOG . 'index.php?route=payment/unitpay/callback';
-		$this->data['copy_success_url']	= HTTP_CATALOG . 'index.php?route=payment/unitpay/success';
-		$this->data['copy_fail_url'] 	= HTTP_CATALOG . 'index.php?route=payment/unitpay/fail';
+		$data['copy_result_url'] 	= HTTP_CATALOG . 'index.php?route=payment/unitpay/callback';
+		$data['copy_success_url']	= HTTP_CATALOG . 'index.php?route=payment/unitpay/success';
+		$data['copy_fail_url'] 	= HTTP_CATALOG . 'index.php?route=payment/unitpay/fail';
 
 		
 
 
 
-		$this->data['entry_order_status'] = $this->language->get('entry_order_status');
-		$this->data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
-		$this->data['entry_status'] = $this->language->get('entry_status');
-		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
+		$data['entry_order_status'] = $this->language->get('entry_order_status');
+		$data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
+		$data['entry_status'] = $this->language->get('entry_status');
+		$data['entry_sort_order'] = $this->language->get('entry_sort_order');
 
-		$this->data['button_save'] = $this->language->get('button_save');
-		$this->data['button_cancel'] = $this->language->get('button_cancel');
+		$data['button_save'] = $this->language->get('button_save');
+		$data['button_cancel'] = $this->language->get('button_cancel');
 
-		$this->data['tab_general'] = $this->language->get('tab_general');
+		$data['tab_general'] = $this->language->get('tab_general');
 
 		//
 	
 if (isset($this->error['warning'])) {
-			$this->data['error_warning'] = $this->error['warning'];
+			$data['error_warning'] = $this->error['warning'];
 		} else {
-			$this->data['error_warning'] = '';
+			$data['error_warning'] = '';
 		}
 
 		//
 		if (isset($this->error['login'])) {
-			$this->data['error_login'] = $this->error['login'];
+			$data['error_login'] = $this->error['login'];
 		} else {
-			$this->data['error_login'] = '';
+			$data['error_login'] = '';
 		}
 
 		if (isset($this->error['password1'])) {
-			$this->data['error_password1'] = $this->error['password1'];
+			$data['error_password1'] = $this->error['password1'];
 		} else {
-			$this->data['error_password1'] = '';
+			$data['error_password1'] = '';
 		}
 
 
 		
-$this->data['action'] = $this->url->link('payment/unitpay', 'token=' . $this->session->data['token'], 'SSL');
+$data['action'] = $this->url->link('payment/unitpay', 'token=' . $this->session->data['token'], 'SSL');
 
-		$this->data['cancel'] = $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL');
+		$data['cancel'] = $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL');
 
-		$this->data['breadcrumbs'] = array();
+		$data['breadcrumbs'] = array();
 
-   		$this->data['breadcrumbs'][] = array(
+   		$data['breadcrumbs'][] = array(
        		'text'      => $this->language->get('text_home'),
 			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
       		'separator' => false
    		);
 
-   		$this->data['breadcrumbs'][] = array(
+   		$data['breadcrumbs'][] = array(
        		'text'      => $this->language->get('text_payment'),
 			'href'      => $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'),
       		'separator' => ' :: '
    		);
 
-   		$this->data['breadcrumbs'][] = array(
+   		$data['breadcrumbs'][] = array(
        		'text'      => $this->language->get('heading_title'),
 			'href'      => $this->url->link('payment/unitpay', 'token=' . $this->session->data['token'], 'SSL'),
       		'separator' => ' :: '
@@ -107,57 +107,55 @@ $this->data['action'] = $this->url->link('payment/unitpay', 'token=' . $this->se
 		
 		//
 		if (isset($this->request->post['unitpay_login'])) {
-			$this->data['unitpay_login'] = $this->request->post['unitpay_login'];
+			$data['unitpay_login'] = $this->request->post['unitpay_login'];
 		} else {
-			$this->data['unitpay_login'] = $this->config->get('unitpay_login');
+			$data['unitpay_login'] = $this->config->get('unitpay_login');
 		}
 
 
 		//
 		if (isset($this->request->post['unitpay_key'])) {
-			$this->data['unitpay_key'] = $this->request->post['unitpay_key'];
+			$data['unitpay_key'] = $this->request->post['unitpay_key'];
 		} else {
-			$this->data['unitpay_key'] = $this->config->get('unitpay_key');
+			$data['unitpay_key'] = $this->config->get('unitpay_key');
 		}
 
 		
 		if (isset($this->request->post['unitpay_order_status_id'])) {
-			$this->data['unitpay_order_status_id'] = $this->request->post['unitpay_order_status_id'];
+			$data['unitpay_order_status_id'] = $this->request->post['unitpay_order_status_id'];
 		} else {
-			$this->data['unitpay_order_status_id'] = $this->config->get('unitpay_order_status_id');
+			$data['unitpay_order_status_id'] = $this->config->get('unitpay_order_status_id');
 		}
 
 		$this->load->model('localisation/order_status');
 
-		$this->data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+		$data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
 
 		if (isset($this->request->post['unitpay_geo_zone_id'])) {
-			$this->data['unitpay_geo_zone_id'] = $this->request->post['unitpay_geo_zone_id'];
+			$data['unitpay_geo_zone_id'] = $this->request->post['unitpay_geo_zone_id'];
 		} else {
-			$this->data['unitpay_geo_zone_id'] = $this->config->get('unitpay_geo_zone_id');
+			$data['unitpay_geo_zone_id'] = $this->config->get('unitpay_geo_zone_id');
 		}
 
 		$this->load->model('localisation/geo_zone');
 
-		$this->data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
+		$data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
 
 		if (isset($this->request->post['unitpay_status'])) {
-			$this->data['unitpay_status'] = $this->request->post['unitpay_status'];
+			$data['unitpay_status'] = $this->request->post['unitpay_status'];
 		} else {
-			$this->data['unitpay_status'] = $this->config->get('unitpay_status');
+			$data['unitpay_status'] = $this->config->get('unitpay_status');
 		}
 
 		if (isset($this->request->post['unitpay_sort_order'])) {
-			$this->data['unitpay_sort_order'] = $this->request->post['unitpay_sort_order'];
+			$data['unitpay_sort_order'] = $this->request->post['unitpay_sort_order'];
 		} else {
-			$this->data['unitpay_sort_order'] = $this->config->get('unitpay_sort_order');
+			$data['unitpay_sort_order'] = $this->config->get('unitpay_sort_order');
 		}
 
-		$this->template = 'payment/unitpay.tpl';
-		$this->children = array(
-			'common/header',
-			'common/footer'
-		);
+        $this->response->setOutput ($this->load->view ('payment/unitpay.tpl', $data));
+        $data['header'] = $this->load->controller('common/header');
+        $data['footer'] = $this->load->controller('common/footer');
 
 		$this->response->setOutput($this->render(TRUE), $this->config->get('config_compression'));
 	}
@@ -176,11 +174,7 @@ $this->data['action'] = $this->url->link('payment/unitpay', 'token=' . $this->se
 		}
 
 	
-		if (!$this->error) {
-			return TRUE;
-		} else {
-			return FALSE;
-		}
+		return !$this->error;
 	}
 }
 ?>
